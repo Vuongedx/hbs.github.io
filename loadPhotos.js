f3631e7af3240f6b30efac2b5d25df4da1a6997b
Vuongedx <75001051+Vuongedx@users.noreply.github.com>
2023-04-28 17:51:52 +0700

    Add vuongedx.github.io to repository

--- loadPhotos.js
+++ loadPhotos.js
@@ -1,33 +1,32 @@
-const fs = require('fs');
+const postContainer = document.querySelector('#post-container');
+const htmlFiles = Array.from(document.querySelectorAll('a[href^="posts/"]'))
+  .map(link => link.getAttribute('href').split('/').pop())
+  .filter(file => file.endsWith('.html'));
 
-fs.readdir('./posts', (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const urls = files
-    .filter(file => file.endsWith('.html'))
-    .map(file => `https://example.com/posts/${file}`);
-
-  for (const url of urls) {
-    const url = url
-    const title = link.textContent.trim();
-    const postElement = document.createElement('div');
-    postElement.classList.add('col-xl-3', 'col-lg-4', 'col-md-6', 'col-sm-6', 'col-12', 'mb-5');
-    postElement.innerHTML = `
-      <figure class="effect-ming tm-video-item">
-        <img src="img/img-03.jpg" alt="Image" class="img-fluid">
-        <figcaption class="d-flex align-items-center justify-content-center">
-          <h2>Kỹ năng sống</h2>
-          <a href="posts/${url}">View more</a>
-        </figcaption>
-      </figure>
-      <div class="d-flex justify-content-between tm-text-gray">
-        <span class="tm-text-gray-light">${new Date().toLocaleDateString()}</span>
-        <span>0 views</span>
-      </div>
-    `;
-    postContainer.appendChild(postElement);
-  }
-});
+fetch('posts')
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(data, 'text/html');
+    const links = html.querySelectorAll('a');
+    for (const [i, link] of links.entries()) {
+      const url = htmlFiles[i].replace('.html', '');
+      const title = link.textContent.trim();
+      const postElement = document.createElement('div');
+      postElement.classList.add('col-xl-3', 'col-lg-4', 'col-md-6', 'col-sm-6', 'col-12', 'mb-5');
+      postElement.innerHTML = `
+        <figure class="effect-ming tm-video-item">
+          <img src="img/img-03.jpg" alt="Image" class="img-fluid">
+          <figcaption class="d-flex align-items-center justify-content-center">
+            <h2>${title}</h2>
+            <a href="posts/${url}">View more</a>
+          </figcaption>
+        </figure>
+        <div class="d-flex justify-content-between tm-text-gray">
+          <span class="tm-text-gray-light">${new Date().toLocaleDateString()}</span>
+          <span>0 views</span>
+        </div>
+      `;
+      postContainer.appendChild(postElement);
+    }
+  });
