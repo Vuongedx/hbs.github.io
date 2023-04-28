659cbd89c804da332b2c12ae047cc0ad3c439eec
Vuongedx <75001051+Vuongedx@users.noreply.github.com>
2023-04-28 17:26:57 +0700

    Add vuongedx.github.io to repository

--- loadPhotos.js
+++ loadPhotos.js
@@ -7,7 +7,7 @@ fetch('posts')
     const html = parser.parseFromString(data, 'text/html');
     const links = html.querySelectorAll('a');
     for (const link of links) {
-      const url = link.href;
+      const url = link.textContent;
       const title = link.textContent;
       const postElement = document.createElement('div');
       postElement.classList.add('col-xl-3', 'col-lg-4', 'col-md-6', 'col-sm-6', 'col-12', 'mb-5');
@@ -16,7 +16,7 @@ fetch('posts')
           <img src="img/img-03.jpg" alt="Image" class="img-fluid">
           <figcaption class="d-flex align-items-center justify-content-center">
             <h2>Kỹ năng sống</h2>
-            <a href="${url}">View more</a>
+            <a href="posts/${url}">View more</a>
           </figcaption>
         </figure>
         <div class="d-flex justify-content-between tm-text-gray">
